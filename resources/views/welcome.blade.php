bfb45028ed72ff8260f3baa2e746bee53f16d6df
ridwanpranata <ridwanpranata10@gmail.com>
2023-02-28 08:09:16 +0700

    add fav ico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Cutting Ticket App</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+        
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
