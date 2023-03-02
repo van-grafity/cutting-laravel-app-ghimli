a549c35220c20c87748bddd301c4d3eeb4eba4f7
ivansuhendra <ivan.suhendraaja@gmail.com>
2023-03-02 09:37:07 +0700

    added laying planing size

---
+++ database/migrations/2023_03_02_020452_create_laying_planing_sizes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('laying_planing_sizes', function (Blueprint $table) {
+            $table->id();
+            $table->string('laying_planing_id');
+            $table->string('size_id');
+            $table->string('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laying_planing_sizes');
+    }
+};
