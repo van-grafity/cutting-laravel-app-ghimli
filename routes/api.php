5fbb4878173c4fee6617a81534be0d3b7072dceb
ivansuhendra <ivan.suhendraaja@gmail.com>
2023-03-15 16:29:22 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,13 @@ use App\Http\Controllers\API\CuttingOrdersController;
 
 Route::post('login', [AuthController::class, 'signin']);
 Route::post('register', [AuthController::class, 'signup']);
-Route::get('/users', [AuthController::class, 'index']);
+
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/auth/logout', [AuthController::class, 'logout']);
-
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/users', [AuthController::class, 'index']);
+    
     Route::resource('blogs', BlogController::class);
     Route::get('cutting-orders', [CuttingOrdersController::class, 'index']);
     Route::get('cutting-orders/{id}', [CuttingOrdersController::class, 'show']);
