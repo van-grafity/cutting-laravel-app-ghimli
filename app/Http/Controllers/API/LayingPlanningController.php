f601adf73b5e71ba210545f5af416c4d56ef74b9
ivansuhendra <ivan.suhendraaja@gmail.com>
2023-04-03 15:15:06 +0700

    Create LayingPlanningController.php

---
+++ app/Http/Controllers/API/LayingPlanningController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Http\Request;
+use App\Models\CuttingOrderRecord;
+use App\Models\CuttingOrderRecordDetail;
+use App\Models\LayingPlanningDetail;
+use App\Models\LayingPlanning;
+use App\Models\Color;
+use App\Models\Remark;
+use App\Http\Traits\ApiHelpers;
+// DB
+use Illuminate\Support\Facades\DB;
+
+class LayingPlanningController extends BaseController
+{
+
+    use ApiHelpers;
+
+    public function index()
+    {
+        $data = LayingPlanningDetail::with('layingPlanning','cuttingOrderRecord','cuttingOrderRecord.cuttingOrderRecordDetail','cuttingOrderRecord.cuttingOrderRecordDetail.color', 'layingPlanning.color')->get();
+        $data = collect(
+            [
+                'laying_planning_detail' => $data
+            ]
+        );
+        return $this->onSuccess($data, 'Laying Planning Detail retrieved successfully.');
+    }
+
+    public function show($serial_number)
+    {
+        $getCuttingOrder = CuttingOrderRecord::with(['CuttingOrderRecordDetail', 'CuttingOrderRecordDetail.color'])->where('serial_number', $serial_number)->latest()->first();
+        if ($getCuttingOrder == null) return $this->onError(404, 'Cutting Order Record not found.');
+        $layingPlanningDetail = LayingPlanningDetail::with(['layingPlanning', 'layingPlanning.color'])->find($getCuttingOrder->layingPlanningDetail->id);
+        if ($layingPlanningDetail->layingPlanning->color == null || $layingPlanningDetail->layingPlanning->color->id == null) return $this->onError(404, 'Color not found.');
+        if ($layingPlanningDetail->marker_yard == null) return $this->onError(404, 'Marker Yard not found.');
+        $data = collect(
+            [
+                // 'cutting_order_record' => $getCuttingOrder,
+                'laying_planning_detail' => $layingPlanningDetail,
+            ]
+        );
+        return $this->onSuccess($data, 'Cutting Order Record retrieved successfully.');
+    }
+}
