258dee2a97a8bbcfd399aa56ef6cdefb7842d36f
ivansuhendra <ivan.suhendraaja@gmail.com>
2023-05-17 10:44:11 +0700

    hiden button cut packinglist

--- resources/views/page/cutting-ticket/report.blade.php
+++ resources/views/page/cutting-ticket/report.blade.php
@@ -9,32 +9,42 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 @php
+
 @endphp
 <body>
     <table class="table table-bordered table-sm">
         <thead>
             <tr>
-                <th scope="col">No</th>
-                <th scope="col">Color</th>
-                @foreach($data['array_size'] as $size)
-                    <th scope="col">{{ $size['size'] }}</th>
-                @endforeach
+                <th rowspan="2">No</th>
+                <th rowspan="2">Color</th>
+                <?php
+                    $size = array();
+                    foreach($cutting_tickets as $ct){
+                        $size[] = $ct->size->size;
+                    }
+                    $size = array_unique($size);
+                    foreach($size as $s){
+                        echo "<th>$s</th>";
+                    }
+                ?>
                 <th scope="col">Quantity</th>
+            </tr>
+            <tr>
+                <?php
+                $layer = array();
+                foreach($cutting_tickets as $ct){
+                    $layer[] = $ct->layer;
+                }
+                $layer = array_unique($layer);
+                foreach($layer as $l){
+                    echo "<th>$l</th>";
+                }
+                ?>
+                <th scope="col"> - </th>
             </tr>
         </thead>
         <tbody>
-            @foreach($data['array_size'] as $size)
-                @foreach($size['data'] as $item)
-                    <tr>
-                        <td>{{ $item->no }}</td>
-                        <td>{{ $item->color }}</td>
-                        @foreach($data['array_size'] as $size)
-                            <td>{{ $item->qty }}</td>
-                        @endforeach
-                        <td>{{ $item->qty }}</td>
-                    </tr>
-                @endforeach
-            @endforeach
+            
         </tbody>
     </table>
 </body>
