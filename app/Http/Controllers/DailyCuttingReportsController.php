b230e5d4bcc436ab627cef80085d42add742c87f
Ivan Suhendra <38711246+van-grafity@users.noreply.github.com>
2023-07-21 16:56:25 +0700

    daily cutting add groups

--- app/Http/Controllers/DailyCuttingReportsController.php
+++ app/Http/Controllers/DailyCuttingReportsController.php
@@ -30,6 +30,16 @@ class DailyCuttingReportsController extends Controller
     public function dailyCuttingReport(Request $request) {
         $date_filter = $request->date;
         
+        // get data group yang ada potong hari ini
+        $groups = Groups::select('groups.id', 'group_name')
+        ->join('user_groups', 'user_groups.group_id', '=', 'groups.id')
+        ->join('users', 'users.id', '=', 'user_groups.user_id')
+        ->join('cutting_order_record_details', 'cutting_order_record_details.operator', '=', 'users.name')
+        ->whereDate('cutting_order_record_details.created_at', '=', $date_filter)
+        ->groupBy('groups.id')
+        ->orderBy('groups.id')
+        ->get();
+
         // get buyer yang potong hari ini
         $buyers = CuttingOrderRecord::select('buyers.*')
         ->join('laying_planning_details', 'laying_planning_details.id', '=', 'cutting_order_records.laying_planning_detail_id')
@@ -45,7 +55,7 @@ class DailyCuttingReportsController extends Controller
 
         foreach($buyers as $key => $buyer) {
             // get laying planning yang potong hari ini
-            $layingPlannings = LayingPlanning::select('buyers.name', 'gls.gl_number', 'colors.color', 'styles.style', 'laying_plannings.order_qty', 'laying_plannings.id as laying_planning_id')
+            $layingPlannings = LayingPlanning::select('buyers.name', 'gls.gl_number', 'colors.color', 'styles.style', 'fabric_cons.name as cons_name', 'laying_plannings.order_qty', 'laying_plannings.id as laying_planning_id')
             ->join('laying_planning_details', 'laying_planning_details.laying_planning_id', '=', 'laying_plannings.id')
             ->join('cutting_order_records', 'cutting_order_records.laying_planning_detail_id', '=', 'laying_planning_details.id')
             ->join('cutting_order_record_details', 'cutting_order_record_details.cutting_order_record_id', '=', 'cutting_order_records.id')
@@ -53,15 +63,53 @@ class DailyCuttingReportsController extends Controller
             ->join('styles', 'styles.id', '=', 'laying_plannings.style_id')
             ->join('buyers', 'buyers.id', '=', 'gls.buyer_id')
             ->join('colors', 'colors.id', '=', 'laying_plannings.color_id')
+            ->join('fabric_cons', 'fabric_cons.id', '=', 'laying_plannings.fabric_cons_id')
             ->whereDate('cutting_order_record_details.created_at', '=', $date_filter)
             ->where('gls.buyer_id', '=', $buyer->id)
             ->groupBy('laying_plannings.id')
             ->get();
-            
             foreach($layingPlannings as $key_lp => $laying_planning) {
                 $total_qty_per_day = 0;
                 $total_previous_cutting = 0;
+                $qty_per_groups = [];
 
+                // get qty per groups
+                foreach($groups as $key_group => $group)
+                {
+                    $qty_group = 0;
+
+                    $cutting_order_record_groups = CuttingOrderRecord::select('cutting_order_records.*', DB::raw("SUM(cutting_order_record_details.layer) as total_layer"))
+                    ->join('laying_planning_details', 'laying_planning_details.id', '=', 'cutting_order_records.laying_planning_detail_id')
+                    ->join('cutting_order_record_details', 'cutting_order_record_details.cutting_order_record_id', '=', 'cutting_order_records.id')
+                    ->join('users', 'users.name', '=', 'cutting_order_record_details.operator')
+                    ->join('user_groups', 'user_groups.user_id', '=', 'users.id')
+                    ->join('groups', 'groups.id', '=', 'user_groups.group_id')
+                    ->where('laying_planning_details.laying_planning_id', '=', $laying_planning->laying_planning_id)
+                    ->whereDate('cutting_order_record_details.created_at', '=', $date_filter)
+                    ->where('groups.id', '=', $group->id)
+                    ->get();
+                    
+                    foreach ($cutting_order_record_groups as $key_cor => $cor) {
+                        
+                        $total_ratio = LayingPlanningDetailSize::select('laying_planning_detail_sizes.*')
+                        ->join('laying_planning_details', 'laying_planning_details.id', '=', 'laying_planning_detail_sizes.laying_planning_detail_id')
+                        ->join('cutting_order_records', 'cutting_order_records.laying_planning_detail_id', '=', 'laying_planning_details.id')
+                        ->where('cutting_order_records.id', '=', $cor->id)
+                        ->sum('laying_planning_detail_sizes.ratio_per_size');
+
+                        $cutting_order_record_groups[$key_cor]->total_size_ratio = $total_ratio;
+                        $cutting_order_record_groups[$key_cor]->total_qty_per_cor = $total_ratio * $cor->total_layer;
+                        $qty_group += $total_ratio * $cor->total_layer;
+                    }
+
+                   $qty_per_groups[$key_group] = (object) [
+                        'group_id' => $group->id,
+                        'group_name' => $group->group_name,
+                        'qty_group' => $qty_group,
+                   ];
+                }
+                $layingPlannings[$key_lp]->qty_per_groups = $qty_per_groups;
+                
                 // get cor dari setiap laying planning
                 $cutting_order_records = CuttingOrderRecord::select('cutting_order_records.*', DB::raw("SUM(cutting_order_record_details.layer) as total_layer"))
                 ->join('laying_planning_details', 'laying_planning_details.id', '=', 'cutting_order_records.laying_planning_detail_id')
@@ -120,9 +168,9 @@ class DailyCuttingReportsController extends Controller
         // dd($data);
         // return $data; 
         
-        $filename = 'Daily Cutting Output Report';
-        $pdf = PDF::loadview('page.daily-cutting-report.print', compact('data', 'date_filter'))->setPaper('a4', 'landscape');
-        return $pdf->stream($filename);
-        // return view('page.daily-cutting-report.print', compact('data', 'date_filter'));
+        // $filename = 'Daily Cutting Output Report';
+        // $pdf = PDF::loadview('page.daily-cutting-report.print', compact('data', 'date_filter', 'groups'))->setPaper('a4', 'landscape');
+        // return $pdf->stream($filename);
+        return view('page.daily-cutting-report.print', compact('data', 'date_filter', 'groups'));
     }
 }
