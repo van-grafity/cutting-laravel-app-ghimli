cdc9da8e5074dafe9745784e2344af2916e9808e
Ivan Suhendra <38711246+van-grafity@users.noreply.github.com>
2023-08-07 15:50:35 +0700

    Update statusCut.php

--- app/Models/statusCut.php
+++ app/Models/statusCut.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class statusCut extends Model
+class StatusCut extends Model
 {
     use HasFactory;
 
