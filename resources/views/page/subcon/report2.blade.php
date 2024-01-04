7b49135a3778f8dc1512978f27781a80b734713d
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2024-01-04 17:17:27 +0700

    show gl number on Summary Cutting Report by Group

--- resources/views/page/subcon/report2.blade.php
+++ resources/views/page/subcon/report2.blade.php
@@ -53,8 +53,9 @@
             <thead>
                 <tr>
                     <th rowspan="2" width="2%">No</th>
-                    <th rowspan="2">COR Serial No.</th>
+                    <th rowspan="2" width="5%">Gl Number</th>
                     <th rowspan="2" width="7%">No</br>Laying</br>Sheet</th>
+                    <th rowspan="2">COR Serial No.</th>
                     <th rowspan="2" width="6.2%">Date</th>
                     <th rowspan="2">Color</th>
                     <th rowspan="2" hidden>Size/Ratio</th>
@@ -74,11 +75,12 @@
                 @foreach ($details as $key => $value)
                     @foreach ($cuttingOrderRecord as $key2 => $value2)
                         @if ($value->id == $value2->laying_planning_detail_id)
-                        
+                            
                             <tr>
                                 <td>{{ $key + 1 }}</td>
-                                <td style="text-align: left; padding-left: 4px !important;">{{ $value2->serial_number }}</td>
+                                <td style="text-align: left; padding-left: 4px !important;">{{ $value->layingPlanning->gl->gl_number }}</td>
                                 <td>{{ $value->no_laying_sheet }}</td>
+                                <td style="text-align: left; padding-left: 4px !important;">{{ $value2->serial_number }}</td>
                                 <td>{{ date('d-m-Y', strtotime($value2->cut)) }}</td>
                                 
                                 <td style="text-align: left; padding-left: 4px !important;">{{ $value->layingPlanning->color->color }}</td>
