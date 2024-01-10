4efc42fb16d24841d915e01287c4ae0fe2e340a4
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2024-01-10 17:14:09 +0700

    open laying planning report for pmr account

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -62,7 +62,7 @@ class AuthServiceProvider extends ServiceProvider
         });
         
         Gate::define('clerk', function ($user) {
-            return $user->hasRole(['super_admin','planner','cutter', 'layer','ticketer','ppc', 'merchandiser', 'bundle']);
+            return $user->hasRole(['super_admin','planner','cutter', 'layer','ticketer','pmr','ppc', 'merchandiser', 'bundle']);
         });
 
         Gate::define('editor', function ($user) {
