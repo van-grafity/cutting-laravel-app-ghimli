d33d01aee43c8dbfd2b9d527cadeb7f5a8a0ccb5
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2024-02-26 17:08:40 +0700

    Fabric Request API. Add start and date filter

--- app/Http/Controllers/API/FabricRequestSyncController.php
+++ app/Http/Controllers/API/FabricRequestSyncController.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
+use Carbon\Carbon;
+
 class FabricRequestSyncController extends Controller
 {
 
@@ -33,45 +35,54 @@ class FabricRequestSyncController extends Controller
 
     public function getFabricRequest(Request $request)
     {
-        $data_input = $request->all();
-        $gl = GL::where('gl_number', $data_input['gl_number'])->first();
-        if ($gl == null) return $this->onError(404, 'GL Not Found!');
+        if ($request->has('start_date') == null) return $this->onError(404, 'Attribute start_date not found!');
+        if ($request->has('end_date') == null) return $this->onError(404, 'Attribute end_date not found!');
+        
+        $start_date =  Carbon::parse($request->start_date)->format('Y-m-d 00:00:00');
+        $end_date =  Carbon::parse($request->end_date)->format('Y-m-d 23:59:59');
+        
         try {
-            // ## Get Laying Planning from GL
-            $laying_plannings = LayingPlanning::where('gl_id',$gl->id)->get();
-            foreach ($laying_plannings as $key => $laying_planning) {
-                $fabric_requests = FabricRequisition::
-                    join('laying_planning_details', 'laying_planning_details.id','=','fabric_requisitions.laying_planning_detail_id')
-                    ->join('laying_plannings','laying_plannings.id','=','laying_planning_details.laying_planning_id')
-                    ->join('gls','gls.id','=','laying_plannings.gl_id')
-                    ->join('colors','colors.id','=','laying_plannings.color_id')
-                    ->where('laying_planning_details.laying_planning_id', $laying_planning->id)
-                    ->select(
-                        'fabric_requisitions.id as fbr_id',
-                        'fabric_requisitions.serial_number as fbr_serial_number',
-                        'fabric_requisitions.status_print as fbr_status_print',
-                        'fabric_requisitions.remark as fbr_remark',
-                        'fabric_requisitions.created_at as fbr_created_at',
-                        'fabric_requisitions.updated_at as fbr_updated_at',
-                        'laying_planning_details.id as laying_planning_detail_id',
-                        'gls.gl_number',
-                        'colors.color',
-                        'laying_planning_details.table_number',
-                        'laying_planning_details.total_length as qty_required',
-                    )
-                    ->get();
-
-                $laying_planning->fabric_requests = $fabric_requests;
+            if($request->gl_number) {
+                $gl = GL::where('gl_number', 'LIKE', "%{$request->gl_number}%")->first();
+            } else {
+                $gl = null;
             }
+            
+            $fabric_requests = FabricRequisition::
+                join('laying_planning_details', 'laying_planning_details.id','=','fabric_requisitions.laying_planning_detail_id')
+                ->join('laying_plannings','laying_plannings.id','=','laying_planning_details.laying_planning_id')
+                ->join('gls','gls.id','=','laying_plannings.gl_id')
+                ->join('colors','colors.id','=','laying_plannings.color_id')
+                ->where(function($query) use ($gl) {
+                    if($gl) {
+                        $query->where('gls.id', $gl->id);
+                    }
+                })
+                ->where('fabric_requisitions.created_at', '>=', $start_date)
+                ->where('fabric_requisitions.created_at', '<=', $end_date)
+                ->select(
+                    'fabric_requisitions.id as fbr_id',
+                    'fabric_requisitions.serial_number as fbr_serial_number',
+                    'fabric_requisitions.status_print as fbr_status_print',
+                    'fabric_requisitions.remark as fbr_remark',
+                    'fabric_requisitions.created_at as fbr_created_at',
+                    'fabric_requisitions.updated_at as fbr_updated_at',
+                    'laying_planning_details.id as laying_planning_detail_id',
+                    'gls.gl_number',
+                    'colors.color',
+                    'laying_planning_details.table_number',
+                    'laying_planning_details.total_length as qty_required',
+                )
+                ->get();
             
         } catch (\Throwable $th) {
             return $this->onError(500, $th->getMessage());
         }
 
         $data_return = [
-            'laying_plannings' => $laying_plannings
+            'fabric_requests' => $fabric_requests
         ];
-        return $this->onSuccess($data_return, 'Success Retrieving Fabric Request of GL ' . $gl->gl_number);
+        return $this->onSuccess($data_return, 'Success Retrieving ' . count($fabric_requests) . ' Fabric Requests');
     }
     
 }
